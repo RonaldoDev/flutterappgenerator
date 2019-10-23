065a44bd8ea3f0b582a3a75d9906964095ba923e
ronaldodev <ronaldospranger@gmail.com>
2019-10-23 01:13:20 -0300

    Fix incremental id problem Fix tab change error create global component id adapt app to use global component count

--- client/src/layoutEditor/nav/nav.selectors.js
+++ client/src/layoutEditor/nav/nav.selectors.js
@@ -2,4 +2,6 @@
 
 export const getViews = store => store.component.views;
 
+export const getLastId = store => store.component.compId;
+
 export const getCurrentTab = store => store.component.currentTab;
